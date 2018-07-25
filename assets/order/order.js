bd972232cc1df1fb85f9dc866e3b6933f2fd8fb9
BussSergey <busssa@yandex.ru>
2018-07-25 16:40:03 +0600

    new file

---
+++ assets/order/order.js
@@ -0,0 +1,18 @@
+/*
+ Copyright (C) Federico Zivolo 2017
+ Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
+ 	function Order(ValueText) {
+	   var NewWin = window.open('order.html');
+		setTimeout(function () {
+	        wrt(NewWin, ValueText);
+	    },1000);
+	}
+	function wrt(ValueObj, ValText) {
+		var obj=ValueObj.document.getElementById('part');
+		if (obj) {
+		    obj.innerHTML=ValText;
+        return;
+	    }
+	}
+
